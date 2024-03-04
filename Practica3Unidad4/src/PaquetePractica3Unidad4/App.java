73ec7525a400697f7c3f56a2a1c1c8106996b4ce
DAW1-A <DAW1-A@PC14>
2024-03-04 18:37:48 +0100

    Realizamos una pequeña suma

--- Practica3Unidad4/src/PaquetePractica3Unidad4/App.java
+++ Practica3Unidad4/src/PaquetePractica3Unidad4/App.java
@@ -2,6 +2,8 @@ package PaquetePractica3Unidad4;
 
 public class App {
 	public static void Main(String[] args) {
-        
+        int num1=5;
+        int num2=6;
+        int suma=num1+num2;
     }
 }
